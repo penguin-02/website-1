c63e01fc3858c33d6b72eaaac8070a5a75cd46d1
penguin <grizzly@Macintosh.local>
2021-10-27 13:13:28 +0900

    jsの追加と修正

--- assets/js/jquery-2.4.js
+++ assets/js/jquery-2.4.js
@@ -1,7 +1,18 @@
 $(function() {
-  $(".area_top_view .container img").fadeIn(2100);
+  $(window).on("load scroll", function() {
+    $(".area_top_view .container img").fadeIn(2100);
 
-  $(".area_top_view .container p").hide().fadeIn(2100);
+    $(".area_top_view .container p").fadeIn(2400);
+
+    $('.fade-in').each(function() {
+      var elemPos = $(this).offset().top,
+        scroll = $(window).scrollTop(),
+        windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight + 100) {
+        $(this).addClass('scrollin');
+      }
+    });
+  });
 
   // モーダルウィンドウを開く
   $('.js-modal-open').on('click', function() {
